ef550e28fe507a8fc60a032c6315d01a1ebee3af
Nicolai Ree <nicolairee@hotmail.com>
2023-12-12 13:12:16 +0100

    Added borane to MAA sites

--- src/esnuel/locate_atom_sites.py
+++ src/esnuel/locate_atom_sites.py
@@ -122,6 +122,7 @@ e_smirks_names_list = ["Oxonium",
                       'double_bond_neighbouratom_with_charge_plus1', # added to capture additional sites
                       'triple_bond', # added to capture additional sites
                       'triple_bond_neighbouratom_with_charge_plus1' # added to capture additional sites
+                      'Borane' # added to capture additional sites
                       ]
 
 e_smirks_list = ['[#6:1]=[O+;!$([O]~[!#6]);!$([S]*~[#7,#8,#15,#16]):2]>>[CH3][#6:1][O+0:2]', 
@@ -140,6 +141,7 @@ e_smirks_list = ['[#6:1]=[O+;!$([O]~[!#6]);!$([S]*~[#7,#8,#15,#16]):2]>>[CH3][#6
                 '[*+0:1]=[*+1:2]>>[CH3][*:1]-[*+0:2]', # added to capture additional sites
                 '[*+0:1]#[*+0:2]>>[CH3][*:1]=[*-1:2]', # added to capture additional sites
                 '[*+0:1]#[*+1:2]>>[CH3][*:1]=[*+0:2]' # added to capture additional sites
+                '[B+0:1]>>[CH3][*-1:1]', # added to capture additional sites
                 ]
 # ### BEGIN SMe (OBS! Require changes to run_rxn() in molecule_formats.py) ###
 # e_smirks_list = ['[#6:1]=[O+;!$([O]~[!#6]);!$([S]*~[#7,#8,#15,#16]):2]>>[CH3][#16X2][#6:1][O+0:2]', 
@@ -158,6 +160,7 @@ e_smirks_list = ['[#6:1]=[O+;!$([O]~[!#6]);!$([S]*~[#7,#8,#15,#16]):2]>>[CH3][#6
 #                 '[*+0:1]=[*+1:2]>>[CH3][#16X2][*:1]-[*+0:2]', # added to capture additional sites
 #                 '[*+0:1]#[*+0:2]>>[CH3][#16X2][*:1]=[*-1:2]', # added to capture additional sites
 #                 '[*+0:1]#[*+1:2]>>[CH3][#16X2][*:1]=[*+0:2]' # added to capture additional sites
+#                 '[B+0:1]>>[CH3][#16X2][*-1:1]', # added to capture additional sites
 #                 ]
 # ### END SMe ###
 
